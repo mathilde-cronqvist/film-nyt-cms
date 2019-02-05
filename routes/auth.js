9bd1958a3ef59c27cb801efbfb815a1a0a7901b4
mathilde-cronqvist <5610151@rts-365.dk>
2019-02-05 10:24:34 +0100

    add opret bruger

--- routes/auth.js
+++ routes/auth.js
@@ -1,13 +1,16 @@
 const db = require('../config/database')();
+const bcrypt = require('bcryptjs');
 
 module.exports = function (app){
+
+	console.log(bcrypt.hashSync('1234', 10));
     app.get('/login', (req, res, next) => {
 		if (req.query.status && req.query.status === 'badcredentials') {
 			console.log("bad badcredentials");
 			res.locals.status = 'ugyldigt brugernavn eller adgangskode';
 		}
 		res.render('login', { title: 'Log ind' });
-	});
+	} );
 
 	app.post('/auth/login', (req, res, next) => {
 			db.query('SELECT id FROM film_nyt.users WHERE username = ? AND passphrase = ?', [req.fields.username, req.fields.passphrase], (err, result) => {
@@ -18,8 +21,11 @@ module.exports = function (app){
 					res.redirect('/login?status=badcredentials');
 					return;
 				}
-				req.session.user = result[0].id;
-				res.redirect('/admin');
+				if(bcrypt.compareSync(req.body.passphrase, result.users.passphrase)){
+					req.session.user = result[0].id;
+					res.redirect('/profile');
+				}
+				
 			});
 	});
 
@@ -27,6 +33,23 @@ module.exports = function (app){
 		req.session.destroy();
 		res.redirect('/');
 	});
+	
+	app.get('/opret', (req, res, next) => {
+		if (req.query.status && req.query.status === 'badcredentials') {
+			console.log("bad badcredentials");
+			res.locals.status = 'ugyldigt brugernavn eller adgangskode';
+		}
+		res.render('opret', { title: 'Opret bruger' });
+	})
+
+	app.post('/auth/opret', (req, res, next) => {
+			console.log("signup commencing");
+			db.query(`INSERT INTO film_nyt.users (username, email, passphrase)
+								VALUES (?, ?, ?) `, [req.fields.username, req.fields.email, req.fields.passphrase], (err, results) => {
+			res.redirect('/profile');
+			});
+	});
+
 	
 
 }
--- routes/profile.js
+++ routes/profile.js
@@ -1,7 +1,7 @@
 const db = require('../config/database')();
 
 module.exports = function(app){
-    app.use('/admin', (req, res, next) => {
+    app.use('/profile', (req, res, next) => {
         if(!req.session.user){
             res.redirect('/login');
             return;
@@ -10,11 +10,11 @@ module.exports = function(app){
         }
     });
 
-    app.get('/admin', (req, res) => {
+    app.get('/profile', (req, res) => {
         db.query(`SELECT * FROM film_nyt.users;`, [req.session.user], function (err, results){
             if (err) return next(`${err} at db.query (${__filename}:15:5)`);
 			console.log(results)
-            res.render('admin', {'title' : 'Admin', 'results' : results});
+            res.render('profile', {'title' : 'Admin', 'results' : results});
         })
     })
 
