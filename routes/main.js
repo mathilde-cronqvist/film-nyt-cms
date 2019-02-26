64a57f0766873872b668a9b859953bf7be9c4106
mathilde-cronqvist <5610151@rts-365.dk>
2019-02-26 14:42:00 +0100

    update article list

--- routes/main.js
+++ routes/main.js
@@ -2,7 +2,9 @@ const db = require('../config/database')();
 
 module.exports = function(app){
     app.get('/', (req, res, next) =>{
-        db.query(`SELECT * FROM film_nyt.articles`, function (err, results) {
+        db.query(`SELECT * FROM film_nyt.articles
+        ORDER BY RAND()
+        LIMIT 2;`, function (err, results) {
             if(err) res.send(err);
             res.render('page', {title : '', 'results' : results});
         })
